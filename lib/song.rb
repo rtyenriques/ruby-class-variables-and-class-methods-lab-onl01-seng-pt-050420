4a8d4d45f76d76b93df157ab1f99392314301aff
Ross Enriques <rtyenriques@gmail.com>
2020-05-24 22:57:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,37 @@
 class Song
-  @@song_count = 0
-def self.count
-  @@song_count
-
+  @@count = 0
+  def.self.count
+    @@count
+  end
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1 
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count
 end
 
-@@song_genres = []
-def self.genres
-  @@genres.uniq
+@@artists = []
+
+def self.artist_count
+  @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
 end
 
-
+def self.artists
+  @@artists.uniq
 end
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @@artists << artist
+  @genre = genre
+  @@genres << genre
+  @@count += 1
+end
+attr_accessor :name, :artist, :genre
+end 
