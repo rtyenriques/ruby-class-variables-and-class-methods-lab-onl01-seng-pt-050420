f751f50d24d4114e0b388f66fb42eeb100c48bc7
Ross Enriques <rtyenriques@gmail.com>
2020-05-24 23:02:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,53 @@
 class Song
-  @@song_count = 0
+  @@count = 0
 
-def initialize
-  @@song_count +=1
-end
+  def self.count
+    @@count
+  end
 
-def self.count
-  @@song_count
-end
+  @@genres = []
 
 
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  #  stackexchange.
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+
+  # This is how the method is done on learn.co:
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  @@artists = []
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
 end
