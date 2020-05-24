79d2bd4460a455f0b3c9e09ca42ed8e206611c51
Ross Enriques <rtyenriques@gmail.com>
2020-05-24 22:57:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
     if genre_count[genre]
-      genre_count[genre] += 1 
+      genre_count[genre] += 1
     else
       genre_count[genre] = 1
     end
@@ -34,4 +34,4 @@ def initialize(name, artist, genre)
   @@count += 1
 end
 attr_accessor :name, :artist, :genre
-end 
+end
